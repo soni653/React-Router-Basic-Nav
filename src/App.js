b97578fd065c0e265dc546a3ced2e854ede6a085
soni653 <dave.family653@outlook.com>
2020-03-12 18:02:58 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
-  <div>
+<div>
+
+<Route exact path="/"/>
+<Route  path="/About"/>
+<Route  path="/Contact"/>
+
     <Navigation />
-  </div>
-);
+</div>
+  
+  
+  
+  );
+
 
 export default App;
